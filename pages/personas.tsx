657a748ddac4c61c606c1b23bd955c8a0c5df854
Jesús Vélez Santiago <jvelezmagic@gmail.com>
2024-04-14 16:02:20 -0600

    chore: Add server-side authentication logic for inactive accounts

--- pages/personas.tsx
+++ pages/personas.tsx
@@ -3,7 +3,32 @@ import * as React from 'react';
 import { AppPersonas } from '../src/apps/personas/AppPersonas';
 
 import { withLayout } from '~/common/layout/withLayout';
+import { type GetServerSideProps } from 'next';
+import { getAuth } from '@clerk/nextjs/server';
+import { db } from '~/server/prisma/db';
 
+// @ts-expect-error leave this alone
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { userId } = getAuth(ctx.req);
+
+  if (userId) {
+    const account = await db.account.findFirst({
+      where: {
+        userId: userId,
+      },
+    });
+
+    if (account?.status === 'INACTIVE') {
+      return {
+        redirect: {
+          destination: '/packages',
+        },
+      };
+    }
+  }
+
+  return { props: {} };
+}
 
 export default function PersonasPage() {
   return withLayout({ type: 'optima' }, <AppPersonas />);
--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -6,6 +6,32 @@ import { Box } from '@mui/joy';
 
 import { withLayout } from '~/common/layout/withLayout';
 
+import { type GetServerSideProps } from 'next';
+import { getAuth } from '@clerk/nextjs/server';
+import { db } from '~/server/prisma/db';
+
+// @ts-expect-error leave this alone
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { userId } = getAuth(ctx.req);
+
+  if (userId) {
+    const account = await db.account.findFirst({
+      where: {
+        userId: userId,
+      },
+    });
+
+    if (account?.status === 'INACTIVE') {
+      return {
+        redirect: {
+          destination: '/packages',
+        },
+      };
+    }
+  }
+
+  return { props: {} };
+}
 
 export default function PersonasPage() {
   return withLayout({ type: 'optima' }, <Box />);
